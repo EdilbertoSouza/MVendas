5b6f27665294fdc3556e2e35f54a95ad9d191607
Edilberto Lima de Souza <edilberto.sss@gmail.com>
2013-10-17 20:47:20 -0300

    Pacote model

---
+++ src/br/com/mvendas/model/Equipamento.java
@@ -0,0 +1,46 @@
+package br.com.mvendas.model;
+
+public class Equipamento {
+
+	private String id;
+	private String sitio;
+	private String name;
+	private String status;
+	private String endereco;
+	
+	public Equipamento(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSitio() {
+		return sitio;
+	}
+	public void setSitio(String sitio) {
+		this.sitio = sitio;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+		
+}
